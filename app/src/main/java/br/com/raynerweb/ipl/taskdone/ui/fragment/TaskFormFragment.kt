ae35604864fe3f0973e8129c82866bb51835af76
raynerweb <raynerweb@gmail.com>
2022-06-25 10:02:59 +0100

    added task form page

--- app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/TaskFormFragment.kt
+++ app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/TaskFormFragment.kt
@@ -1,60 +1,39 @@
 package br.com.raynerweb.ipl.taskdone.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.raynerweb.ipl.taskdone.R
+import android.widget.RadioGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.raynerweb.ipl.taskdone.databinding.FragmentTaskFormBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TaskFormFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TaskFormFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentTaskFormBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_form, container, false)
+    ): View {
+        binding = FragmentTaskFormBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TaskFormFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TaskFormFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    fun statusSelected(radioGroup: RadioGroup, id: Int) {
+        when (id) {
+            binding.rbBacklog.id -> Log.d("TASK", "BACKLOG")
+            binding.rbProgress.id -> Log.d("TASK", "IN PROGRESS")
+            binding.rbCompleted.id -> Log.d("TASK", "COMPLETED")
+        }
     }
+
+    fun save(view: View) {
+        findNavController().navigateUp()
+    }
+
 }
