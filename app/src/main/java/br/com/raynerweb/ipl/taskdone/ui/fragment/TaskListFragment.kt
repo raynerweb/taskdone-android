fb407b66078ad182097e202c5e92b9827c492d69
raynerweb <raynerweb@gmail.com>
2022-06-25 09:45:01 +0100

    fragment binding

--- app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/TaskListFragment.kt
+++ app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/TaskListFragment.kt
@@ -1,20 +1,30 @@
 package br.com.raynerweb.ipl.taskdone.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import br.com.raynerweb.ipl.taskdone.R
+import br.com.raynerweb.ipl.taskdone.databinding.FragmentTaskListBinding
 
 class TaskListFragment : Fragment() {
+
+    private lateinit var binding: FragmentTaskListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    ): View {
+        binding = FragmentTaskListBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    fun addTask(view: View) {
+        findNavController().navigate(R.id.action_taskListFragment_to_taskFormFragment)
     }
 
 }
