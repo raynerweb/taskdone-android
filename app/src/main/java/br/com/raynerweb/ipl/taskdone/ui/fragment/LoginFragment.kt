edeb9ec627f4b88a4e3521ab46a44c7b6913d43d
raynerweb <raynerweb@gmail.com>
2022-07-09 22:11:34 +0100

    partial login

--- app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/LoginFragment.kt
+++ app/src/main/java/br/com/raynerweb/ipl/taskdone/ui/fragment/LoginFragment.kt
@@ -1,46 +1,95 @@
 package br.com.raynerweb.ipl.taskdone.ui.fragment
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import br.com.raynerweb.ipl.taskdone.R
+import br.com.raynerweb.ipl.taskdone.databinding.FragmentLoginBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-//    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var binding: FragmentLoginBinding
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
     private lateinit var oneTapClient: SignInClient
     private lateinit var signInRequest: BeginSignInRequest
+
+    private val gso: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("")
+
+            .requestEmail()
+            .requestProfile()
+            .build()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        oneTapClient = Identity.getSignInClient(requireActivity())
-        signInRequest = BeginSignInRequest.builder()
-            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-                .setSupported(true)
-                .build())
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-//                    .setServerClientId(getString(R.string.your_web_client_id))
-                    // Only show accounts previously used to sign in.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build())
-            // Automatically sign in when exactly one credential is retrieved.
-            .setAutoSelectEnabled(true)
-            .build()
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.fragment = this
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+    }
+
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val acct = GoogleSignIn.getLastSignedInAccount(requireActivity())
+            if (acct != null) {
+                val personName = acct.displayName
+                val personGivenName = acct.givenName
+                val personFamilyName = acct.familyName
+                val personEmail = acct.email
+                val personId = acct.id
+                val personPhoto: Uri? = acct.photoUrl
+            }
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            task.result.account?.let { account ->
+                Log.d(TAG, account.toString())
+            }
+//            when (result.resultCode) {
+//                RESULT_OK -> {
+//                    Log.d(TAG, result.toString())
+//                }
+//                else -> {
+//                    Log.d(TAG, result.toString())
+//                }
+//            }
+        }
+
+    fun loginGoogle(view: View) {
+//        val acct = GoogleSignIn.getLastSignedInAccount(requireActivity())
+//        if (acct != null) {
+//            val personName = acct.displayName
+//            val personGivenName = acct.givenName
+//            val personFamilyName = acct.familyName
+//            val personEmail = acct.email
+//            val personId = acct.id
+//            val personPhoto: Uri? = acct.photoUrl
+//        }
+        val signInIntent = mGoogleSignInClient.signInIntent
+        resultLauncher.launch(signInIntent)
     }
 
     companion object {
